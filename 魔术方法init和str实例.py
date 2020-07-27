5db0086fbe4cdb0e9a670b4621243ba2cc6cccd1
程东雪 <1609611001@qq.com>
2020-07-27 20:23:39 +0800

    魔术方法__init__和__str__

---
+++ 魔术方法init和str实例.py
@@ -0,0 +1,10 @@
+
+class SgClass:
+    def __init__(self,name,color):
+        self.color=color
+        self.name=name
+        pass
+    def __str__(self):
+        return '%s 的颜色是 %s'%(self.name,self.color)
+pg=SgClass('苹果','红色')
+print(pg)
